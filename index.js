cf21b2c85833a14042c5fd3a2a59335386c9bac0
ManukMelkonyan <myan.manuk@gmail.com>
2022-05-26 19:16:48 +0400

    feature/ added execution time limit logic (probably the worst solution for first :D)

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
-// const child_process = require('child_process');
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 
 const exec = require('child_process').exec;
-
-
-
-const sourceCode = `
-    '5';
-    console.log(typeof('5'));
-    let x = 10;
-    console.log(x + '220');
-    console.log("what the hell is going on");
-`;
-
 
 app.use(express.json());
 
 app.post('/submit', (req, res) => {
     const upContainerAndRunCode = (code, language) => {
-        exec(`docker run -e SOURCE_CODE="$SOURCE_CODE" ${language}-runtime:latest`, {env: {"SOURCE_CODE": code}},  (err, stdout, stderr) => {
+        const containerName = language + '-' + uuidv4();
+        console.log({ containerName });
+        let isTimeout = false;
+        let responseSent = false;
+        exec(`docker run --rm -e SOURCE_CODE="$SOURCE_CODE" --name '${containerName}' ${language}-runtime:latest`, {env: {"SOURCE_CODE": code}},  (err, stdout, stderr) => {
             if(err) {
-                console.error(err);
-                res.send(stderr);
+                if (isTimeout) return;
+                console.error("Error", err);
+
+                if (!responseSent) {
+                    res.send(stderr);
+                    responseSent = true;
+                }
             }
             else {
                 console.log(stdout);
-                res.send(stdout);
-            }   
+                if (!responseSent) {
+                    res.send(stdout);
+                    responseSent = true;
+                }
+            }
+        });
+        exec(`sleep 5`, (err, stdout, stderr) => {
+            if (responseSent) return;
+            if(err) {
+                console.error(err);
+            }
+            else {
+                console.log(stdout);
+            }
+            res.send("Execution time limit exceeded");
+            responseSent = true;
+            isTimeout = true;
+            exec(`docker stop ${containerName}`, (err, stdout, stderr) => {
+                if(err) {
+                    console.log("error while stopping container\n", err);
+                }
+                else {
+                    console.log('succesfully stopped container');
+                }
+
+            })
         });
     }
     upContainerAndRunCode(req.body.code, req.body.language);
